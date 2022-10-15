4ad58a62108864f9fd83e88857622cd3dabd94ec
sieuneee <kseun021@naver.com>
2022-10-15 23:29:44 +0900

    erona-domain

--- erona-domain/src/main/java/com/sg/erona/domain/EronaDomainApplication.java
+++ erona-domain/src/main/java/com/sg/erona/domain/EronaDomainApplication.java
@@ -1,4 +1,10 @@
 package com.sg.erona.domain;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableAsync
 public class EronaDomainApplication {
 }
